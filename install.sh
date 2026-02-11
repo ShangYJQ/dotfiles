2e70367fe57a113a17e8343f1b8e595e3c8e90f1
ShangYJQ <421207553@qq.com>
2026-02-12 02:03:48 +0800

    use zoxide

--- install.sh
+++ install.sh
@@ -3,38 +3,39 @@
 set -Eeuo pipefail
 
 PACMAN_PKGS=(
-    noto-fonts-cjk
-    noto-fonts-emoji
-    ttf-cascadia-code-nerd
-    fcitx5-im
-    fcitx5-chinese-addons
-    fcitx5-qt
-    fcitx5-gtk
-    papirus-icon-theme
-    qt6ct
-    hypridle
-    eza
-    kitty
-    fzf
-    git
-    curl
-    zsh
-    zsh-autosuggestions
-    zsh-syntax-highlighting
-    qt6-svg
-    qt6-virtualkeyboard
-    qt6-multimedia-ffmpeg
-    seahorse
-    cliphist
-    wl-clipboard
+	noto-fonts-cjk
+	noto-fonts-emoji
+	ttf-cascadia-code-nerd
+	fcitx5-im
+	fcitx5-chinese-addons
+	fcitx5-qt
+	fcitx5-gtk
+	papirus-icon-theme
+	qt6ct
+	hypridle
+	eza
+	kitty
+	fzf
+	git
+	curl
+	zsh
+	zsh-autosuggestions
+	zsh-syntax-highlighting
+	qt6-svg
+	qt6-virtualkeyboard
+	qt6-multimedia-ffmpeg
+	seahorse
+	cliphist
+	wl-clipboard
+	zoxide
 )
 
 AUR_PKGS=(
-    neovim-nightly-bin
-    gpu-screen-recorder
-    noctalia-shell
-    sddm-silent-theme-git
-    bibata-cursor-theme-bin
+	neovim-nightly-bin
+	gpu-screen-recorder
+	noctalia-shell
+	sddm-silent-theme-git
+	bibata-cursor-theme-bin
 )
 
 echo ">> 准备安装所需的软件和依赖"
@@ -44,11 +45,11 @@ echo ">> 安装软件包"
 
 sudo pacman -S --needed --noconfirm "${PACMAN_PKGS[@]}"
 
-if command -v paru &> /dev/null; then
-    echo ">> 安装 AUR 软件..."
-    paru -S --needed --noconfirm --skipreview "${AUR_PKGS[@]}"
+if command -v paru &>/dev/null; then
+	echo ">> 安装 AUR 软件..."
+	paru -S --needed --noconfirm --skipreview "${AUR_PKGS[@]}"
 else
-    echo "!! 未检测到 Paru，跳过 AUR 安装。"
+	echo "!! 未检测到 Paru，跳过 AUR 安装。"
 fi
 
 echo ">> 安装完成！"
